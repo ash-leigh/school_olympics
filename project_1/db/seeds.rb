9a5f9c689e26098c4593f8880557fa37327a40ae
Ashleigh Adams <ashleighadams318@hotmail.co.uk>
2016-07-02 21:33:38 +0100

    medals adding to athletes

--- project_1/db/seeds.rb
+++ project_1/db/seeds.rb
@@ -8,17 +8,20 @@ require_relative('../models/athletes_events')
 
 nation_1 = Nation.new({'name' => 'Scotland'}).save
 
-medal_1 = Medal.new("Gold")
+gold_medal = Medal.new("Gold")
+silver_medal = Medal.new("Silver")
+bronze_medal = Medal.new("Bronze")
 
-athlete_1 = Athlete.new({'name' => 'Ashleigh', 'nation_id' => nation_1.id, 'medals' => [medal_1]}).save
-athlete_2 = Athlete.new({'name' => 'John', 'nation_id' => nation_1.id, 'medals' => [medal_1]}).save
+athlete_1 = Athlete.new({'name' => 'Ashleigh', 'nation_id' => nation_1.id, 'medals' => [gold_medal]}).save
+athlete_2 = Athlete.new({'name' => 'John', 'nation_id' => nation_1.id, 'medals' => [gold_medal]}).save
+athlete_3 = Athlete.new({'name' => 'Shaun', 'nation_id' => nation_1.id, 'medals' => [gold_medal]}).save
 
 event_1 = Event.new({'type' => 'egg and spoon race'}).save
 event_2 = Event.new({'type' => 'hula hooping'}).save
 event_3 = Event.new({'type' => 'three legged race'}).save
 
 athletes_events_1 = AthletesEvents.new({'athlete_id' => athlete_1.id, 'event_id' => event_1.id, 'athlete_finishing_position' => 1}).save
-athletes_events_2 = AthletesEvents.new({'athlete_id' => athlete_2.id, 'event_id' => event_1.id, 'athlete_finishing_position' => 1}).save
+athletes_events_2 = AthletesEvents.new({'athlete_id' => athlete_1.id, 'event_id' => event_2.id, 'athlete_finishing_position' => 2}).save
 athletes_events_3 = AthletesEvents.new({'athlete_id' => athlete_1.id, 'event_id' => event_3.id, 'athlete_finishing_position' => 1}).save
 
 binding.pry
