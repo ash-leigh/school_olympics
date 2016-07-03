bf551db2c82e777b8c863219a047ebd2e1af166d
Ashleigh Adams <ashleighadams318@hotmail.co.uk>
2016-07-03 22:58:44 +0100

    medals added to athlete

--- project_1/db/seeds.rb
+++ project_1/db/seeds.rb
@@ -21,9 +21,9 @@ event_1 = Event.new({'type' => 'egg and spoon race'}).save
 event_2 = Event.new({'type' => 'hula hooping'}).save
 event_3 = Event.new({'type' => 'three legged race'}).save
 
-athletes_events_1 = AthletesEvents.new({'athlete_id' => athlete_1.id, 'event_id' => event_1.id, 'athlete_finishing_position' => 1}).save
+athletes_events_1 = AthletesEvents.new({'athlete_id' => athlete_1.id, 'event_id' => event_1.id, 'athlete_finishing_position' => 2}).save
 athletes_events_2 = AthletesEvents.new({'athlete_id' => athlete_1.id, 'event_id' => event_2.id, 'athlete_finishing_position' => 2}).save
-athletes_events_3 = AthletesEvents.new({'athlete_id' => athlete_1.id, 'event_id' => event_3.id, 'athlete_finishing_position' => 3}).save
+athletes_events_3 = AthletesEvents.new({'athlete_id' => athlete_1.id, 'event_id' => event_3.id, 'athlete_finishing_position' => 2}).save
 
 binding.pry
 nil
